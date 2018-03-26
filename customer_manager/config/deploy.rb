d8abb7b5f727f30f309826938b75a4f2d70a935e
Aram Sperduti Menocci <aramsm3@gmail.com>
2018-03-26 05:00:44 -0300

    Deploy structure with Capistrano

--- customer_manager/config/deploy.rb
+++ customer_manager/config/deploy.rb
@@ -1,8 +1,11 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "customer_manager"
+set :repo_url, "git@github.com:aramsm/vagas.git"
+
+set :user, "deployer"
+set :stages, %w(production staging)
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -24,7 +27,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- customer_manager/config/deploy/production.rb
+++ customer_manager/config/deploy/production.rb
@@ -20,8 +20,12 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
-
+role :app, %w{deployer@0.0.0.0:3000}
+role :web, %w{deployer@0.0.0.0:3000}
+role :db, %w{deployer@0.0.0.0:3000}, primary: true
+set :branch, "master"
+set :rails_env, "staging"
+set :deploy_to, "var/www/customer_manager"
 
 # Configuration
 # =============
